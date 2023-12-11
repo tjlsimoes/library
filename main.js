1170e83a79029c1d0611b6560b7df3abd66a8cfe
tjlsimoes <118819479+tjlsimoes@users.noreply.github.com>
2023-12-11 16:25:58 +0000

    Add dialog for user input of a new book
    As of yet non-functional: not possible to add book to myLibrary array.

--- main.js
+++ main.js
@@ -46,3 +46,20 @@ function displayLibrary(myLibrary) {
 }
 
 displayLibrary(myLibrary);
+
+
+
+const dialog = document.querySelector("dialog");
+const showButton = document.querySelector("dialog + button");
+const closeButton = document.querySelector("dialog button");
+
+// "Show the dialog" button opens the dialog modally
+showButton.addEventListener("click", () => {
+  dialog.showModal();
+});
+
+// "Close" button closes the dialog
+closeButton.addEventListener("click", () => {
+  dialog.close();
+});
+
