e203203e13350be516cee5751ab7aea8f445d71c
tjlsimoes <118819479+tjlsimoes@users.noreply.github.com>
2023-12-11 15:58:10 +0000

    Add table to display books from Library

---
+++ main.js
@@ -0,0 +1,48 @@
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+	this.title = title;
+	this.author = author;
+	this.pages = pages;
+	this.read = read;
+	this.info = getInfo(title, author, pages, read)
+
+	function getInfo(title, author, pages, read) {
+		info =`${title} by ${author}, ${pages} pages, `;
+		if (read == true) {
+			info += 'read';
+		} else {
+			info += 'not read yet';
+		}
+		return info;
+	}
+}
+
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+}
+
+theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, true);
+addBookToLibrary(theHobbit);
+annaKarenina = new Book('Anna Karenina', 'Tolstoy', 799, false);
+addBookToLibrary(annaKarenina);
+
+function displayLibrary(myLibrary) {
+  let table = document.getElementById("myLibrary");
+
+  myLibrary.forEach(book => {
+    let row = table.insertRow(-1);
+
+    let c0 = row.insertCell(0);
+    let c1 = row.insertCell(1);
+    let c2 = row.insertCell(2);
+    let c3 = row.insertCell(3);
+
+    c0.innerText = book.title
+    c1.innerText = book.author
+    c2.innerText = book.pages
+    c3.innerText = book.read
+  });
+}
+
+displayLibrary(myLibrary);
